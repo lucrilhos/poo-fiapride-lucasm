8648f0eae1d52a3a7544ea9d571de6f625801c57
luquinha <lucaztimoraes71@gmail.com>
2026-05-10 16:14:16 -0300

    feat: verificar tipo da jaqueta e metodo de desconto

--- src/br/com/fiapride/model/Jaqueta.java
+++ src/br/com/fiapride/model/Jaqueta.java
@@ -1,10 +1,24 @@
 package br.com.fiapride.model;
 
 public class Jaqueta {
+    public String modelo;
+    public String cor;
+    public String tamanho;
+    public double preco;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Método da Aula 2: Aplica desconto
+    public void aplicarDesconto(double porcentagem) {
+        if (porcentagem > 0 && porcentagem <= 100) {
+            this.preco -= this.preco * (porcentagem / 100);
+        }
+    }
 
-	}
-
+    // Método da Aula 2: Retorna a categoria
+    public String verificarCategoria() {
+        if (this.preco > 500.0) {
+            return "Categoria: Premium";
+        } else {
+            return "Categoria: Padrão";
+        }
+    }
 }
